90c6dfb670afaf4a8c334ecb283795e4738d9b55
GracieE <gracie.elqura@gmail.com>
2017-01-08 19:43:58 -0800

    Update visualize.js

--- visualize.js
+++ visualize.js
@@ -55,4 +55,103 @@ $(document).ready(function() {
     }
 
     // PUT D3 CODE HERE, use three arrays (cost, time, distance) above
+    
+    var data = cost;
+
+	var width = 420,
+	    barHeight = 30;
+
+	var x = d3.scale.linear()
+	    .domain([0, d3.max(data)])
+	    .range([0, width]);
+
+
+	//cost chart
+	var y = d3.scale.linear()
+    	.range([barHeight, 0]);
+
+	var yAxis = d3.svg.axis()
+    	.scale(y)
+    	.orient("left");
+
+	var chart = d3.select(".cost")
+	    .attr("width", width)
+	    .attr("height", barHeight * data.length);
+
+	var bar = chart.selectAll("g")
+	    .data(data)
+	  	.enter().append("g")
+	    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+	
+	chart.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis);
+
+
+	bar.append("rect")
+	    .attr("width", x)
+	    .attr("height", barHeight - 1);
+
+	bar.append("text")
+	    .attr("x", function(d) { return x(d) - 3; })
+	    .attr("y", barHeight / 2)
+	    .attr("dy", ".35em")
+	    .text(function(d) { return d; });
+
+	
+	
+	//time chart
+
+	var data = time;
+	var x = d3.scale.linear()
+	    .domain([0, d3.max(data)])
+	    .range([0, width]);
+
+	var chart = d3.select(".time")
+	    .attr("width", width)
+	    .attr("height", barHeight * data.length);
+
+	var bar = chart.selectAll("g")
+	    .data(data)
+	  .enter().append("g")
+	    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+	bar.append("rect")
+	    .attr("width", x)
+	    .attr("height", barHeight - 1);
+
+	bar.append("text")
+	    .attr("x", function(d) { return x(d) - 3; })
+	    .attr("y", barHeight / 2)
+	    .attr("dy", ".35em")
+	    .text(function(d) { return d; });
+
+
+
+	//distance chart
+	var data = distance;
+	var x = d3.scale.linear()
+	    .domain([0, d3.max(data)])
+	    .range([0, width]);
+
+
+	var chart = d3.select(".distance")
+	    .attr("width", width)
+	    .attr("height", barHeight * data.length);
+
+	var bar = chart.selectAll("g")
+	    .data(data)
+	  .enter().append("g")
+	    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+	bar.append("rect")
+	    .attr("width", x)
+	    .attr("height", barHeight - 1);
+
+	bar.append("text")
+	    .attr("x", function(d) { return x(d) - 3; })
+	    .attr("y", barHeight / 2)
+	    .attr("dy", ".35em")
+	    .text(function(d) { return d; });
+    
 });
